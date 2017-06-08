b1147d936876b472815973b62745f17582b2ba7e
Muhammad Syaiful Sabril <syaiful.sabril@gmail.com>
2017-06-08 03:20:20 +0000

    installasi devise

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -30,6 +30,8 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = false
 
   config.action_mailer.perform_caching = false
+  
+  config.action_mailer.default_url_options = { host: 'https://kampus-ror-sabril.c9users.io', port: 80 }
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
