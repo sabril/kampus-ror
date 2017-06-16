7825942e18fd422e554b5898cae0bb1c423dfec8
Muhammad Syaiful Sabril <syaiful.sabril@gmail.com>
2017-06-16 10:28:40 +0000

    konfigurasi email

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -27,11 +27,19 @@ Rails.application.configure do
   end
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
 
   config.action_mailer.perform_caching = false
   
-  config.action_mailer.default_url_options = { host: 'https://kampus-ror-sabril.c9users.io', port: 80 }
+  config.action_mailer.default_url_options = { host: 'https://kampus-ror-sabril.c9users.io' }
+  config.action_mailer.smtp_settings = {
+    :address              => 'smtp.gmail.com',
+    :port                 => 587,
+    :user_name            => ENV['gmail_user_name'],
+    :password             => ENV['gmail_password'],
+    :authentication => :plain,
+    :enable_starttls_auto => true
+  }
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
