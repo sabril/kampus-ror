25117dda127af94aac87618919a2570ecc56e78f
Muhammad Syaiful Sabril <syaiful.sabril@gmail.com>
2017-07-08 00:18:38 +0000

    course and task management

--- app/admin/course.rb
+++ app/admin/course.rb
@@ -1,15 +1,28 @@
 ActiveAdmin.register Course do
-# See permitted parameters documentation:
-# https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-#
-permit_params :title, :description, :price, :status, :image
-#
-# or
-#
-# permit_params do
-#   permitted = [:permitted, :attributes]
-#   permitted << :other if params[:action] == 'create' && current_user.admin?
-#   permitted
-# end
-
+  permit_params :title, :description, :price, :status, :image
+  
+  show do
+    attributes_table do
+      row :title
+      row :description
+      row :price do
+        number_to_currency(course.price)
+      end
+      row :status
+      row :image do
+        course.image.present? ? image_tag(course.image.url, height: 300) : content_tag(:span, 'No Image')
+      end
+    end
+  end
+  
+  form do |f|
+    f.inputs do
+      f.input :title
+      f.input :description
+      f.input :price
+      f.input :status, as: :select, collection: ["Active", "Not Active"], include_blank: false
+      f.input :image, hint: course.image.present? ? image_tag(course.image.url, height: 100) : content_tag(:span, 'No Image')
+    end
+    f.actions
+  end
 end
--- app/models/course.rb
+++ app/models/course.rb
@@ -1,3 +1,10 @@
 class Course < ApplicationRecord
-    has_many :tasks, dependent: :destroy
+  has_many :tasks, dependent: :destroy
+  
+  validates :title, presence: true, length: { maximum: 50 }
+  validates :description, presence: true
+  validates :price, presence: true, numericality: { only_integer: true }
+  
+  has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>" }
+  validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 end
