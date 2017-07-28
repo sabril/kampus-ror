42802a4fd777365b54edc8359e70ffb6dd5287c5
Muhammad Syaiful Sabril <syaiful.sabril@gmail.com>
2017-07-28 15:47:59 +0000

    add coverage and unit test

--- Gemfile
+++ Gemfile
@@ -67,3 +67,4 @@ gem "paperclip", git: "git://github.com/thoughtbot/paperclip.git"
 gem 'cancancan', '~> 2.0'
 
 gem 'friendly_id', '~> 5.1.0'
+gem 'simplecov', :require => false, :group => :test
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,3 +1,5 @@
+require 'simplecov'
+SimpleCov.start 'rails'
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
 class ActiveSupport::TestCase
@@ -5,4 +7,13 @@ class ActiveSupport::TestCase
   fixtures :all
 
   # Add more helper methods to be used by all tests here...
+  def init_omniauth_data
+    OmniAuth.config.test_mode = true
+    OmniAuth.config.mock_auth[:facebook] = OmniAuth::AuthHash.new({
+      provider: 'facebook',
+      uid: '123545',
+      info: {email: "test-from-facebook@test.com", name: "test"},
+      extra: {raw_info: ""}
+    })
+  end
 end
