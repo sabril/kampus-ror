e62e3f232cf4fe5826bb735908427f5010486c89
Muhammad Syaiful Sabril <syaiful.sabril@gmail.com>
2017-08-24 14:45:38 +0000

    action cable

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -1,4 +1,14 @@
 module ApplicationCable
   class Connection < ActionCable::Connection::Base
+    identified_by :current_user
+ 
+    def connect
+      self.current_user = find_verified_user
+    end
+ 
+    private
+      def find_verified_user
+        User.find_by(id: cookies.encrypted[:user_id]) || AdminUser.find_by(id: cookies.encrypted[:user_id])
+      end
   end
 end
